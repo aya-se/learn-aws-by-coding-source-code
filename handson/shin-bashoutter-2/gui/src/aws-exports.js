3fb6b021a736fa7af02ebab79947359ad1a0925c
aya-se <kakkeno1@gmail.com>
2023-07-25 00:23:05 +0900

    shin-bashoutter-2の修正

--- handson/shin-bashoutter-2/gui/src/aws-exports.js
+++ handson/shin-bashoutter-2/gui/src/aws-exports.js
@@ -1,8 +1,8 @@
 module.exports = {
-    Auth: {
-        region: 'ap-northeast-1',
-        userPoolId: 'ap-northeast-1_YRr0aJ2Mx',
-        userPoolWebClientId: '2jumfc5t1napiihes45s37mptk',
-        authenticationFlowType: 'USER_PASSWORD_AUTH',
-    }
-}
+  Auth: {
+    region: "ap-northeast-1",
+    userPoolId: "ap-northeast-1_QaXor2k6B",
+    userPoolWebClientId: "50jj5p9658ldqmresct701fmkq",
+    authenticationFlowType: "USER_PASSWORD_AUTH",
+  },
+};
